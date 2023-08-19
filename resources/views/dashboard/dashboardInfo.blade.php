83581178043e6025741055297c54f505a47d3734
Cgyulas0525 <cseszneki.gyula@gmail.com>
2023-08-19 10:19:38 +0200

    commit RM-2 DADADA

--- resources/views/dashboard/dashboardInfo.blade.php
+++ resources/views/dashboard/dashboardInfo.blade.php
@@ -13,7 +13,7 @@
             <div class="col-lg-6 col-md-6 col-xs-12">
                 <div class="info-box mb-3 bg-danger">
                     <div class="info-box-content">
-                        <span class="h4"> {{ __( 'Adat törlés BABABA' ) }}:</span>
+                        <span class="h4"> {{ __( 'Adat törlés DADADA' ) }}:</span>
                         <h1><a href="#" class="btn btn-default btn-block" id="truncateButton">{{ __('Indít') }}</a></h1>
                     </div>
                     <!-- /.info-box-content -->
